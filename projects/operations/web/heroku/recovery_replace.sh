7fcaf84dfb5dae48c45801f056893c5a09298975
AliciaYoobic <amarin@yoobic.com>
2018-06-01 11:40:04 +0100

    refactor(operations): remove unnecessary files

--- projects/operations/web/heroku/recovery_replace.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if [[ -n $NEW_STRING ]] ; then
-  echo ${REPLACE_FILES-main.bundle.js main.bundle.js.map} | xargs -t -P ${CONCURRENCY-1} -n 1 sed -i'.bkp' -e 's|'"${OLD_STRING//./\.}"'|'"$NEW_STRING"'|g'
-fi
--- projects/operations/web/onesignal/OneSignalSDKUpdaterWorker.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-/* eslint no-undef:0 */
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
