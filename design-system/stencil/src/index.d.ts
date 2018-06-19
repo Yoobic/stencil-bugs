9950d8cd5066e59b21ea90aaf5c422995732c1b6
tkiddleYoobic <tkiddle@yoobic.com>
2018-06-19 13:55:23 +0100

    chore(stencil): Remove unused stencil components

--- design-system/stencil/src/index.d.ts
+++ design-system/stencil/src/index.d.ts
@@ -4,63 +4,6 @@ export interface Window {
     mapboxgl: any;
 }
 
-// APP components
-export { YooFeedCreateComponent } from './components/app/feed-create/feed-create';
-export { YooLanguageSelectorComponent } from './components/app/language-selector/language-selector';
-export { YooLoginComponent } from './components/app/login/login';
-export { YooProfileComponent } from './components/app/profile/profile';
-export { YooResetPasswordComponent } from './components/app/reset-password/reset-password';
-export { YooWalkthroughComponent } from './components/app/walkthrough/walkthrough';
-export { YooChatCreateComponent } from './components/app/chat-create/chat-create';
-export { YooEventCreateComponent } from './components/app/event-create/event-create';
-export { YooServiceCreateComponent } from './components/app/service-create/service-create';
-export { YooTaskCreateComponent } from './components/app/task-create/task-create';
-export { YooSearchPageComponent } from './components/app/search-page/search-page';
-
-// CORE components
-export { YooAccordionComponent } from './components/core/accordion/accordion';
-export { YooActionSheetComponent } from './components/core/action-sheet/action-sheet';
-export { YooAlertComponent } from './components/core/alert/alert';
-export { YooAvatarComponent } from './components/core/avatar/avatar';
-export { YooBadgeComponent } from './components/core/badge/badge';
-export { YooBreadcrumbsComponent } from './components/core/breadcrumbs/breadcrumbs';
-export { YooButtonComponent } from './components/core/button/button';
-export { YooButtonGroupComponent } from './components/core/button-group/button-group';
-export { YooCalendarComponent } from './components/core/calendar/calendar';
-export { YooChatComponent } from './components/core/chat/chat';
-export { YooContextMenuComponent } from './components/core/context-menu/context-menu';
-export { YooDeviceComponent } from './components/core/device/device';
-export { YooEmptyStateComponent } from './components/core/empty-state/empty-state';
-export { YooFabButtonComponent } from './components/core/fab-button/fab-button';
-export { YooFabContainerComponent } from './components/core/fab-container/fab-container';
-export { YooFabListComponent } from './components/core/fab-list/fab-list';
-export { YooLoaderComponent } from './components/core/loader/loader';
-export { YooModalComponent } from './components/core/modal/modal';
-export { YooModalControllerComponent } from './components/core/modal-controller/modal-controller';
-export { YooNavbarComponent } from './components/core/navbar/navbar';
-export { YooPaginationComponent } from './components/core/pagination/pagination';
-export { YooPanelComponent } from './components/core/panel/panel';
-export { YooProgressBarComponent } from './components/core/progress-bar/progress-bar';
-export { YooScrollSpyComponent } from './components/core/scroll-spy/scroll-spy';
-export { YooSlimScrollComponent } from './components/core/slim-scroll/slim-scroll';
-export { YooStickyComponent } from './components/core/sticky/sticky';
-export { YooTabsComponent } from './components/core/tabs/tabs';
-export { YooToolbarComponent } from './components/core/toolbar/toolbar';
-export { YooTooltipComponent } from './components/core/tooltip/tooltip';
-export { YooTransitionComponent } from './components/core/transition/transition';
-export { YooVerticalMenuComponent } from './components/core/vertical-menu/vertical-menu';
-export { YooZoomComponent } from './components/core/zoom/zoom';
-
-//ENTITIES components
-export { YooCardComponent } from './components/entities/card/card';
-export { YooCardFeedComponent } from './components/entities/card-feed/card-feed';
-export { YooCardStickyComponent } from './components/entities/card-sticky/card-sticky';
-export { YooCardListComponent } from './components/entities/card-list/card-list';
-export { YooEntityComponent } from './components/entities/entity/entity';
-export { YooFeedCommentsComponent } from './components/entities/feed-comments/feed-comments';
-export { YooMissionDetailComponent } from './components/entities/mission-detail/mission-detail';
-export { YooMissionResultsComponent } from './components/entities/mission-results/mission-results';
-
 // FORM components
 export { YooFormAutocompleteComponent } from './components/form/form-autocomplete/form-autocomplete';
 export { YooFormAutocompleteDialogComponent } from './components/form/form-autocomplete/form-autocomplete-dialog';
